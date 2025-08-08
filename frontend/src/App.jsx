c2bd36357e9dbb2f753cb4249dfb81645152eedb
Kamran Khan <kamranmuhammad12k@@gmail.com>
2025-08-08 21:56:09 +0500

    final web completion commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { v4 as uuidV4 } from 'uuid';
+import TextEditor from './TextEditor';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate replace to={`/documents/${uuidV4()}`} />}
+        />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
